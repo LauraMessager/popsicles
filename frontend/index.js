9be748ba86a6c931e3e84b7f5973153db7e78932
LauraMessager <150282637+LauraMessager@users.noreply.github.com>
2023-11-22 14:47:25 +0100

    front end of the popsicle calculator

---
+++ frontend/index.js
@@ -0,0 +1,23 @@
+/*var valuePopsicle = document.getElementById("value-popsicle");
+var valueSibling = document.getElementById("value-sibling");
+var btn = document.getElementById("calculator-btn");
+var afficherResultat = document.getElementById("afficher-resultat");
+
+const calculatePopsicle = () => {
+var siblings = parseInt(valueSibling.value);
+var popsicles = parseInt(valuePopsicle.value);
+var res = popsicles % siblings;
+
+console.log("Siblings:", siblings);
+console.log("Popsicles:", popsicles);
+console.log("Result:", res);
+
+if (res === 0){
+    return afficherResultat.textContent = "Oh No! Give Away"
+} else {
+    return afficherResultat.textContent = "Eat Them All!"
+}
+    
+}
+
+btn.addEventListener("click", calculatePopsicle);*/
